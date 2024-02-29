6ba5c4e87f4de8cc3889923e08f9540925278dd2
Bernardo Dartora <bernardintd@gmail.com>
2024-02-29 15:06:28 -0300

    minhaBola

--- modelagem-de-classes/index.php
+++ modelagem-de-classes/index.php
@@ -1,10 +1,22 @@
 <?php
+
+//------------------- 1 --------------------
 require('./contador.php');
 
 $meuContador = new contador();
 
 $meuContador->incrementar();
 $meuContador->valorContador();
+
+//------------------- 2 --------------------
+require('./modelar-bola.php');
+$minhaBola = new bolinha("Roxa", "Redonda", "Madeira");
+
+$minhaBola->mostrarCor();
+$minhaBola->trocarCor("Verde");
+$minhaBola->mostrarCor();
+
+//------------------- 3 --------------------
 
 
 
---
+++ modelagem-de-classes/modelar-bola.php
@@ -0,0 +1,25 @@
+<?php
+
+class bolinha {
+  public $cor;
+  public $circunferência;
+  public $material;
+
+  public function __construct($cor, $circunferência, $material){
+    $this->cor = $cor;
+    $this->circunferência = $circunferência;
+    $this->material = $material;
+  }
+
+  public function trocarCor($cor){
+    $this->cor = $cor;
+  }
+
+  public function mostrarCor(){
+    echo "</br>";
+    echo "</br>";
+    echo "Mostar cor: " . $this->cor;
+
+  }
+  
+}
